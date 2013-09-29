81a56e1b32acb6f9f63a00aa7aae2164ebe27dc3
wangxiang <wangxiang@chufa.la>
2013-09-30 00:09:46 +0800

    post data

--- code/post/formdata_server.js
+++ code/post/formdata_server.js
@@ -2,6 +2,45 @@ var http = require('http'),
 	fs = require('fs'),
 	IP = '127.0.0.1', 
 	PORT = 1337;
+	
+
+function handlerPost(req,postData){
+	var headers = req.headers,
+		contentType = headers['content-type'],
+		reBoundary = /boundary=([^=]*)/,
+		reData = /\r\nContent\-Disposition: form\-data; name="([^=":\-]*)"(?:; filename="([^=":\-]*)"\r\nContent\-Type: ([^:]*))?\r\n\r\n([\s\S]*)\r\n/
+		m = null,
+		boundary = '',
+		list = null;
+		//\r\nContent-Disposition: form-data; name="txt"\r\n\r\ntxtvalue\r\n
+	m = contentType.match(reBoundary);
+	
+	if(m && (boundary = m[1])){
+		list = postData.split('--'+boundary);
+		//console.log(list);
+		list.forEach(function(v){
+			//console.log(v.match(reData));
+			var m = v.match(reData);
+			if(m && m[3] && m[3].indexOf('image/') == 0){
+				//console.log(m[4]);
+				//TODO 
+				/*
+				
+				var out = fs.createWriteStream('out.jpg');
+				out.write(Buffer(m[4],m[4].length));
+				out.end();
+				*/
+				
+				/*
+				var dataBuffer = new Buffer(m[4]);
+				fs.writeFile("out.jpg", dataBuffer, function(err) {
+				    
+				});
+				*/
+			} //m[4] ==> value
+		});
+	}
+}
 
 function handler(req, res) {
 	if (req.method == 'POST') {
@@ -10,7 +49,9 @@ function handler(req, res) {
 	    	body += data;
 	    });
 	    req.on('end', function () {
-			console.log(body);
+			//console.log(req);
+			//console.log(body);
+			handlerPost(req,body);
 			res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
 			res.end();
 	    });
