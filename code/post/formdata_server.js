41a0897a57a3589344d63e579cd6aba6bc463183
wangxiang <wangxiang@chufa.la>
2013-09-30 15:17:37 +0800

    接收数据

--- code/post/formdata_server.js
+++ code/post/formdata_server.js
@@ -64,13 +64,6 @@ function handler(req, res) {
 			//console.log(req);
 			//console.log(body);
 			//handlerPost(req,body);
-			var form = new formidable.IncomingForm();
-
-			form.parse(req, function(err, fields, files) {
-			   res.writeHead(200, {'content-type': 'text/plain'});
-			   res.write('received upload:\n\n');
-			   res.end(util.inspect({fields: fields, files: files}));
-			});
 				
 		//res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
 		//res.end();
